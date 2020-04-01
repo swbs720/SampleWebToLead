cfdb7cbd16c6e8a34ab6e47b17fd9002e9bde1e4
Akiko Hori <ahori@usernoMacBook-puro.local>
2020-04-01 18:15:48 +0900

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+//ルーティング設定
+app.get('/thanks', function (req, res) {
+  res.send('Thank you!! 登録ありがとう！！');
+});
+
+// Serve your app
+console.log('Served: http://localhost:' + port);
+app.listen(port);
