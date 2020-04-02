caf02a0062349ca54eb30cd2278d920aa9945def
Akiko Hori <ahori@usernoMacBook-puro.local>
2020-04-02 20:26:10 +0900

    認証系修正

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: node server.js
-web: serve public --auth
--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+// basic auth
+const basicAuth = require('basic-auth-connect')
+const USERNAME = 'to-nen'
+const PASSWORD = 'to-nen-test'
+
+app.use(basicAuth(USERNAME, PASSWORD))
+
 // Serve static files
 app.use(express.static(__dirname + '/public'));
 
