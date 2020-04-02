675bd8986c74bf36bf79fb565cf5611d1654ab04
Akiko Hori <ahori@usernoMacBook-puro.local>
2020-04-02 21:56:30 +0900

    修正

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 var express = require('express');
+const basicAuth = require('basic-auth-connect');
 var app = express();
 var port = process.env.PORT || 8080;
 
-// basic auth
-const basicAuth = require('basic-auth-connect');
-const USERNAME = 'to-nen';
-const PASSWORD = 'to-nen-test';
-
-app.use(basicAuth(USERNAME, PASSWORD));
-
 // Serve static files
 app.use(express.static(__dirname + '/public'));
+
+// basic auth
+app.use(basicAuth('to-nen', 'to-nen-test'));
 
 //ルーティング設定
 app.get('/thanks', function (req, res) {
